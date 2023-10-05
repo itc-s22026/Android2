e7a7e37a70acedd69b3f8281fff70edab8c18670
itc-s22026 <s22026@std.it_college.ac.jp>
2023-10-05 11:21:30 +0900

    11.6 終わり

--- AsyncSample/app/src/main/java/jp/ac/it_college/std/s22026/asyncsample/MainActivity.kt
+++ AsyncSample/app/src/main/java/jp/ac/it_college/std/s22026/asyncsample/MainActivity.kt
@@ -3,9 +3,11 @@ package jp.ac.it_college.std.s22026.asyncsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
 import androidx.recyclerview.widget.LinearLayoutManager
 import jp.ac.it_college.std.s22026.asyncsample.databinding.ActivityMainBinding
+import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
 import java.net.URL
@@ -41,7 +43,22 @@ class MainActivity : AppCompatActivity() {
         val backgroundReceiver = WeatherInfoBackgroundReceiver(url)
         val future = executorService.submit(backgroundReceiver)
         val result = future.get()
-        binding.tvWeatherDesc.text = result
+        showWeatherInfo(result)
+    }
+
+    @UiThread
+    private fun showWeatherInfo(result: String) {
+        val root = JSONObject(result)
+        val cityName = root.getString("name")
+        val coord = root.getJSONObject("coord")
+        val latitude = coord.getDouble("lat")
+        val longitude = coord.getDouble("lon")
+        val weatherArray = root.getJSONArray("weather")
+        val current = weatherArray.getJSONObject(0)
+        val weather = current.getString("description")
+        // 以下、表示処理
+        binding.tvWeatherTelop.text = getString(R.string.tv_telop, cityName)
+        binding.tvWeatherDesc.text = getString(R.string.tv_desc, weather, latitude, longitude)
     }
 
     private class WeatherInfoBackgroundReceiver(val urlString: String): Callable<String> {
