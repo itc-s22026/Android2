e7a1e5bda09819b5f33d627d968a917daa4cd809
itc-s22026 <s22026@std.it_college.ac.jp>
2023-10-10 11:25:21 +0900

    AsyncSample と同じ

--- KtorSample/app/src/main/java/jp/ac/it_college/std/s22026/ktorsample/MainActivity.kt
+++ KtorSample/app/src/main/java/jp/ac/it_college/std/s22026/ktorsample/MainActivity.kt
@@ -2,10 +2,29 @@ package jp.ac.it_college.std.s22026.ktorsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import jp.ac.it_college.std.s22026.ktorsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val DEBUG_TAG = "AsyncSample"
+        private const val WEATHER_INFO_URL =
+            "https://api.openweathermap.org/data/2.5/weather?lang=ja"
+        private const val APP_ID = BuildConfig.APP_ID
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvCityList.apply {
+            adapter = CityAdapter {
+                // 後でデータ取得の処理を実装したメソッドを呼び出す。
+            }
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 }
