f88c01ad0bb346978624a7f1c1d499eebd075a0a
itc-s22026 <s22026@std.it_college.ac.jp>
2023-10-10 10:32:37 +0900

    initial commit for ext11 chap11

--- KtorSample/app/build.gradle.kts
+++ KtorSample/app/build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    kotlin("plugin.serialization")
 }
 
 android {
-    namespace = "jp.ac.it_college.std.s22026.asynccoroutinesample"
+    namespace = "jp.ac.it_college.std.s22026.ktorsample"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "jp.ac.it_college.std.s22026.asynccoroutinesample"
+        applicationId = "jp.ac.it_college.std.s22026.ktorsample"
         minSdk = 29
         targetSdk = 33
         versionCode = 1
@@ -48,6 +49,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("io.ktor:ktor-client-core:2.3.5")
+    implementation("io.ktor:ktor-client-cio:2.3.5")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.5")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
