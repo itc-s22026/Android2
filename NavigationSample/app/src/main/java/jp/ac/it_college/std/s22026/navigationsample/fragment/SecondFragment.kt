af4f3fb6746acc293241a7fea15da77e34f53b36
itc-s22026 <s22026@std.it_college.ac.jp>
2023-10-25 12:42:04 +0900

    画面間で値の受け渡し(不完全感)

--- NavigationSample/app/src/main/java/jp/ac/it_college/std/s22026/navigationsample/fragment/SecondFragment.kt
+++ NavigationSample/app/src/main/java/jp/ac/it_college/std/s22026/navigationsample/fragment/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import jp.ac.it_college.std.s22026.navigationsample.R
 import jp.ac.it_college.std.s22026.navigationsample.databinding.FragmentHomeBinding
 import jp.ac.it_college.std.s22026.navigationsample.databinding.FragmentSecondBinding
@@ -26,9 +28,20 @@ class SecondFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.choice1.setOnClickListener { toNext(1) }
+        binding.choice2.setOnClickListener { toNext(2) }
+        binding.choice3.setOnClickListener { toNext(3) }
+        binding.choice4.setOnClickListener { toNext(4) }
+    }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+    private fun toNext(choice: Int) {
+        val bundle = bundleOf("choice" to choice)
+        findNavController().navigate(R.id.action_secondFragment_to_thirdFragment, bundle)
     }
 }
---
+++ NavigationSample/app/src/main/java/jp/ac/it_college/std/s22026/navigationsample/fragment/ThirdFragment.kt
@@ -0,0 +1,44 @@
+package jp.ac.it_college.std.s22026.navigationsample.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import jp.ac.it_college.std.s22026.navigationsample.R
+import jp.ac.it_college.std.s22026.navigationsample.databinding.FragmentThirdBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ThirdFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ThirdFragment : Fragment() {
+    private var _binding: FragmentThirdBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val choice = arguments?.getInt("choice", 0)
+        binding.display.text = "<${choice}>"
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
